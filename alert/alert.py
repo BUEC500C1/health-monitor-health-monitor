be4b8d131c2836edb44c2c99390a360bc41357f9
Sebastian Nevarez <snevarez@bu.edu>
2020-03-26 21:57:50 -0400

    created stubs for alert

--- alert/alert.py
+++ alert/alert.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from flask_restful import Resource, Api
 
+import functions as f
+
 app = Flask(__name__)
 api = Api(app)
 
 class alert(Resource):
     def get(self):
-        return {"alert": "module"}
+        alert = f.alert_module()
+        oxygen = alert.get_oxygen()
+        bp = alert.get_bp()
+        pulse = alert.get_pulse()
+        return {"oxygen": oxygen, "blood pressure": bp, "pulse": pulse}
 
 api.add_resource(alert, '/')
 
---
+++ alert/functions.py
@@ -0,0 +1,17 @@
+
+class alert_module():
+
+    def __init__(self):
+        pass
+
+    def get_oxygen(self):
+        oxy = 5
+        return oxy
+
+    def get_bp(self):
+        bp = 7
+        return bp
+
+    def get_pulse(self):
+        pulse = 3
+        return pulse
