d66a3f54ab48e2302d724a6e9fd72d4d0c263e46
Erin Thomas <erin.thomas@Alan-Turing.local>
2020-04-01 22:02:23 -0400

    Calling database functions (not working)

--- Display/app.py
+++ Display/app.py
@@ -1,16 +1,27 @@
 from flask import Flask, render_template
-import database as db 
+from database import createBPtable
+from database import createO2table
+from database import createPulsetable
+from database import getBPdata
+from database import getO2data
+from database import getPulsedata
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
 
+    # Creates database tables
+    createO2table()
+    createBPtable()
+    createPulsetable()
+
     # Gets most recent values from database
     oxygen = getO2data()
     blood_pressure = getBPdata()
     pulse = getPulsedata()
-    return render_template("display.html", oxygen=oxygen, bp=blood_pressure, pulse=pulse);   
+
+    return render_template("display.html", oxygen=oxygen, bp=blood_pressure, pulse=pulse)  
 
 
 if __name__ == "__main__":
